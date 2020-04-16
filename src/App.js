7a556d6d5131e469f57a3d0fa28c9ead58d28599
Jason Goode <jason@carmera.com>
2020-04-15 20:51:35 -0400

    Minor edits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+// import clock from './clock.svg';
 import './App.css';
 
 function App() {
@@ -10,17 +11,6 @@ function App() {
         <p>
           Coming Soon.
         </p>
-        <p>
-          Another Test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
